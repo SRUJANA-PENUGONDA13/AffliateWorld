3e0703522b01292977c0d30d6a8a56dc334209e7
SRUJANA PENUGONDA <srujanapenugonda1318@outlook.com>
2021-01-09 22:38:59 +0530

    First Commit

--- app.py
+++ app.py
@@ -8,22 +8,19 @@ app = Flask(__name__)
 def api_root():
     return render_template('index.html')
 
-@app.route('/redirectedURL/<urlVal>')
-def redirectedURL(urlVal):
-   return render_template('redirectedURL.html', url_val=urlVal)
-
-@app.route('/urlRedirection', methods=['POST'])
+@app.route('/urlRedirection', methods=['GET','POST'])
 def transform_url():
-    input_url = request.form['amazonURL']
+    input_url = "https://amzn.to/2JTVwMj"
+    print("Input URL")
     print(input_url)
     affiliate_code = "&tag=anupt-21&"
     if "amzn.to" in input_url:
-        response = requests.get(input_url)
+        response = requests.get('https://amzn.to/2JTVwMj')
         redirected_url = response.url
     elif "amazon.in" in input_url:
         redirected_url = input_url
     else:
-        return redirect(url_for('error',urlVal = '{"Response": 500, "Message": "Invalid Url"}'))
+        return render_template('errorURL.html', url_val='{"Response": 500, "Message": "Invalid Url"}')
     if "&tag" in redirected_url:
         affiliate_name = re.search(r'&tag=(.*?)&', redirected_url).group(1)
         affiliate_name = "&tag" + affiliate_name + "&"
@@ -31,11 +28,10 @@ def transform_url():
             affiliate_name, affiliate_code)
     else:
         redirected_url = redirected_url + affiliate_code
-    return redirect(url_for('redirectedURL',urlVal = redirected_url))
+    print(redirected_url)
+    return render_template('redirectedURL.html', url_val=redirected_url)
 
-@app.route('/error/<urlVal>')
-def error(urlVal):
-    return render_template('errorURL.html', url_val=urlVal)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
